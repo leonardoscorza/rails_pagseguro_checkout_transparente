0c55c59f6a1f191da540f5735b09cd5bf61faa3c
Leonardo Scorza <leonardoscorzas88@gmail.com>
2017-01-06 19:02:22 -0200

    PagSeguro

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -1,4 +1,15 @@
 class NotificationController < ApplicationController
+  skip_before_filter :authenticate_user!, :only => :create
   def create
+    transaction = PagSeguro::Transaction.find_by_notification_code(params[:notificationCode])
+    status = ['Aguardando Pagamento', 'Em análise', 'Paga', 'Disponível', 'Em disputa', 'Devolvida', 'Cancelada']
+
+    if transaction.errors.empty?
+      order = Order.where(reference: transaction.reference).last
+      order.status = status[transaction.status.id.to_i - 1]
+      order.save
+    end
+
+      render nothing: true, status: 200
   end
 end

928236d4662b3c7253ba28b47b21485159b8ef42
Leonardo Scorza <leonardoscorzas88@gmail.com>
2017-01-06 22:52:41 -0200

    PagSeguro

--- config/initializers/pagseguro.rb
+++ config/initializers/pagseguro.rb
@@ -1,6 +1,6 @@
 PagSeguro.configure do |config|
-  config.token       = "4A90009EF4184B30951E2201F685582C"
-  config.email       = "leonardoscorzas@hotmail.com"
+  config.token       = "seu_token_do_pagseguro"
+  config.email       = "seu_email_do_pagseguro"
   config.environment = :sandbox # ou :sandbox. O padrão é production.
   config.encoding    = "UTF-8" # ou ISO-8859-1. O padrão é UTF-8.
 end
